a5e175f034d5121898ae0e5119aa0ab8baf21df6
Michael Governanti <michaelgovernanti@gmail.com>
2025-04-09 16:25:31 -0400

    nmap scans

--- scripts/Scan-Drexel.ps1
+++ scripts/Scan-Drexel.ps1
@@ -1,3 +1,3 @@
 
-$targets = Get-Content -Path .\subdomains.txt -ErrorAction Stop
+$targets = Get-Content -Path ..\subdomains.txt -ErrorAction Stop
 $targets | ForEach-Object { sudo nmap -p 1-65535 $_ -A -v -oN ("./scans/$_-nmap.txt") }
